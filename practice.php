f08276ad332fe369e1666d5af30e945fdffd9dda
y-hirade <yuuki.h.0909@gmail.com>
2023-01-31 16:25:40 +0000

    PHP05 課題

--- practice.php
+++ practice.php
@@ -1,28 +1,32 @@
 <?php
-echo 'hello php!';
-$a = 3;
-$b = 7;
-echo $a + $b;
-$array_manth =["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
-echo $array_manth[7];
-$hello = "Hello";
 $name = "Yuki";
-$world = "World!";
-echo $hello.$name."'s".$world;
-$tech_boost = "tech";
-$tech_boost .= " boost";
-echo $tech_boost;
-$calendar_2018 = [
-    "January" => "1月",
-    "February" => "2月",
-    "March" => "3月",
-    "April" => "4月",
-    "May" => "5月",
-    "June" => "6月",
-    "July" => "7月",
-    "August" => "8月",
-    "September" => "9月",
-    "October" => "10月",
-    "November" => "11月",
-    "December" => "12月"];
-echo $calendar_2018["December"];    
+if ($name = "Yuki"){
+    echo "私は ".$name." です";
+}
+else{
+    echo "私は".$name."ではありません";
+}
+echo "\n";
+$total = 1;
+for ($i = 1; $i <= 10000; $i++){
+    $total += $i;
+}
+echo $total;
+echo "\n";
+$fruits = ["banana", "apple", "strawberry", "grape", "melon"];
+foreach ($fruits as $fluet){
+    if($fluet == "melon"){
+        echo $fluet . "";
+        }
+        else{
+            echo $fluet . ", ";
+        }
+}
+echo "\n";
+$start = 1;
+$end = 100;
+for ($i = $start; $i <= $end; $i++){
+    if ($i % 5 ==0){
+        echo $i."\n";
+        }
+}
